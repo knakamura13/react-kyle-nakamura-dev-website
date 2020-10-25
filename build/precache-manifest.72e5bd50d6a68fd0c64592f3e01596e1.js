046f4711d4d70f3a4a720baab8103d79e2c68b35
Kyle Nakamura <kjnakamura@apu.edu>
2020-10-24 19:59:05 -0700

    Testing new configurations of npm start scripts

---
+++ build/precache-manifest.72e5bd50d6a68fd0c64592f3e01596e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95411f419ab3064d5c07a368638bf158",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea97a2635cf5702399d2",
+    "url": "/static/js/2.5882d6e6.chunk.js"
+  },
+  {
+    "revision": "dce7e1788e37969512284cfb62d3c5f8",
+    "url": "/static/js/2.5882d6e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4db119e3ea9987ed8fcb",
+    "url": "/static/js/main.5422502b.chunk.js"
+  },
+  {
+    "revision": "18d3082d2ceaf3f69195",
+    "url": "/static/js/runtime-main.06d629e6.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+
+    console.log("App.js constructor()");
   }
 
   render() {
