065b8663eb9a08a9daacbe91db8e8a4713d24814
Kyle Nakamura <kjnakamura@apu.edu>
2020-10-26 08:34:28 -0700

    Style home with animated shape greeting

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,50 @@
-import Header from "./Header";
+import { useState, useEffect } from "react";
 
 /****************
  Component Class
 *****************/
 
-const Home = () => (
-    <div className="component" id="home">
-        {/*<Header title="Kyle Nakamura" />*/}
-        <div>
-            <label>new website, who dis?</label>
+const Home = () => {
+    const [X, setX] = useState(""),
+        [Y, setY] = useState('');
+
+    let offset = {
+        transform: `translate(-50%, -50%) perspective(600px)
+                    rotateY(${X}deg)
+                    rotateX(${Y}deg)`
+    }
+
+    useEffect(() => {
+        const friction = 1/32;
+
+        const mouseMove = (e) => {
+            let followX = window.innerWidth / 2 - e.clientX;
+            let followY = window.innerHeight / 2 - e.clientY;
+
+            let x = -followX * friction;
+            let y = followY * friction;
+
+            setX(x);
+            setY(y);
+        }
+
+        window.addEventListener('mousemove', mouseMove);
+
+        // Returned function will be called on component unmount
+        return () => {
+            window.removeEventListener('mousemove', mouseMove)
+        }
+    }, [])
+
+    return (
+        <div className="component" id="home">
+            <div className='wrapper' style={offset}>
+                <div className="shape"></div>
+                <div className="shape2"><p>A website by Kyle Nakamura</p></div>
+                <p>new website, who dis?</p>
+            </div>
         </div>
-    </div>
-);
+    );
+};
     
 export default Home;
